3ec8d180427e14fe4c57565bd7d278d2be4ffd6d
Ypurek <oleksii.ostapov@gmail.com>
2021-02-15 16:08:49 +0200

    added test git

--- tests/test_testcases.py
+++ tests/test_testcases.py
@@ -21,3 +21,7 @@ def test_new_testcase(desktop_app_auth, name, description):
 def test_testcase_does_not_exist(desktop_app_auth):
     desktop_app_auth.navigate_to('Test Cases')
     assert not desktop_app_auth.test_cases.check_test_exists('fndsfidsnisdfnisdfdsf')
+
+
+def test_git():
+    assert True
