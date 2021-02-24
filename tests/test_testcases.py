afa60db02b5ee1b42c21a27f850907ab5ec10a49
Ypurek <oleksii.ostapov@gmail.com>
2021-02-24 13:36:13 +0200

    moved assert to last record in all tests

--- tests/test_testcases.py
+++ tests/test_testcases.py
@@ -15,10 +15,12 @@ def test_new_testcase(desktop_app_auth, name, description, get_db):
     desktop_app_auth.navigate_to('Create new test')
     desktop_app_auth.create_test(name, description)
     desktop_app_auth.navigate_to('Test Cases')
-    assert desktop_app_auth.test_cases.check_test_exists(name)
-    assert len(tests) + 1 == len(get_db.list_test_cases())
-    get_db.delete_test_case(name)
+    test_exists = desktop_app_auth.test_cases.check_test_exists(name)
+    tests_new_count = get_db.list_test_cases()
     # desktop_app_auth.test_cases.delete_test_by_name(name)
+    get_db.delete_test_case(name)
+    assert test_exists
+    assert len(tests) + 1 == len(tests_new_count)
 
 
 def test_testcase_does_not_exist(desktop_app_auth):
@@ -30,6 +32,7 @@ def test_delete_test_case(desktop_app_auth, get_web_service):
     test_name = 'test for delete'
     get_web_service.create_test(test_name, 'delete me pls')
     desktop_app_auth.navigate_to('Test Cases')
-    assert desktop_app_auth.test_cases.check_test_exists(test_name)
+    test_exists = desktop_app_auth.test_cases.check_test_exists(test_name)
     desktop_app_auth.test_cases.delete_test_by_name(test_name)
+    assert test_exists
     assert not desktop_app_auth.test_cases.check_test_exists(test_name)
